eaa9b0e489ff02ba2ef7384f107991c8d43b266c
eric <eric.g.mcdaniel@gmail.com>
2025-04-22 10:56:55 -0400

    regl

--- apps/dunk/plugins/render/plugin.ts
+++ apps/dunk/plugins/render/plugin.ts
@@ -1,241 +1,78 @@
-import {Controls, PerspectiveCamera} from "cameras"
-import {ControlsOptions} from "cameras/types/types"
-import {
-  Attachment,
-  Attribute,
-  BindGroup,
-  BindGroupLayout,
-  Buffer,
-  Command,
-  Context,
-  GPUIndexFormat,
-  GPUShaderStage,
-  Pass,
-  Pipeline,
-  Sampler,
-  Texture,
-  Uniform,
-  WGSLBuiltIn,
-} from "dgel"
-import {mat4} from "gl-matrix"
-import {cube} from "primitive-geometry"
-import {App, Selector, Range, Component, System} from "silver-ecs"
-import typed_array_concat from "typed-array-concat"
-import typed_array_interleave from "typed-array-interleave"
+import {App, Range, Selector, System} from "silver-ecs"
 import {IsPlayer} from "../player/plugin"
+import Regl from "regl"
+import {Time} from "silver-ecs/plugins"
 
-// context
+let regl = Regl()
 
-let context = new Context()
-let context_init = context.init(undefined, undefined, undefined, {
-  glslangPath: "/assets/glslang.js",
-  twgslPath: "/assets/twgsl.js",
-})
-
-if (!(await context_init)) {
-  throw new Error("failed to initialize WebGPU")
+type Uniforms = {
+  color: string
 }
 
-document.body.appendChild(context.canvas)
+// Calling regl() creates a new partially evaluated draw command
+const drawTriangle = regl<Uniforms>({
+  // Shaders in regl are just strings.  You can use glslify or whatever you want
+  // to define them.  No need to manually create shader objects.
+  frag: `
+    precision mediump float;
+    uniform vec4 color;
+    void main() {
+      gl_FragColor = color;
+    }`,
 
-// camera
+  vert: `
+    precision mediump float;
+    attribute vec2 position;
+    void main() {
+      gl_Position = vec4(position, 0, 1);
+    }`,
 
-let camera = new PerspectiveCamera({
-  position: [3, 3, 3],
-  fov: Math.PI / 2,
-  aspect: 1,
-  near: 0.1,
-  far: 1000,
+  // Here we define the vertex attributes for the above shader
+  attributes: {
+    // regl.buffer creates a new array buffer object
+    position: regl.buffer([
+      [-2, -2], // no need to flatten nested arrays, regl automatically
+      [4, -2], // unrolls them into a typedarray (default Float32)
+      [4, 4],
+    ]),
+    // regl automatically infers sane defaults for the vertex attribute pointers
+  },
+
+  uniforms: {
+    // This defines the color of the triangle to be a dynamic variable
+    color: regl.prop<Uniforms, "color">("color"),
+  },
+
+  // This tells regl the number of vertices to draw in this command
+  count: 3,
 })
-let camera_controls = new Controls({
-  element: context.canvas,
-  camera,
-  position: camera.position,
-  target: camera.target,
-  distanceBounds: [camera.near, camera.far],
-} as unknown as ControlsOptions)
 
-camera.update()
-
-let on_resize = () => {
-  context.resize(window.innerWidth, window.innerHeight)
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-}
+let on_resize = () => {}
 on_resize()
 
-// layouts
-
-let system_bind_group_layout = new BindGroupLayout([
-  {
-    buffer: {},
-    visibility: GPUShaderStage.VERTEX,
-    name: "App.",
-    uniforms: [
-      new Uniform("projection_matrix", "mat4"),
-      new Uniform("view_matrix", "mat4"),
-    ],
-  },
-])
-
-let mesh_bind_group_layout = new BindGroupLayout([
-  {
-    buffer: {},
-    visibility: GPUShaderStage.VERTEX,
-    name: "Mesh",
-    uniforms: [new Uniform("model_matrix", "mat4")],
-  },
-  {
-    sampler: {},
-    visibility: GPUShaderStage.FRAGMENT,
-    name: "u_sampler",
-  },
-  {
-    texture: {},
-    visibility: GPUShaderStage.FRAGMENT,
-    name: "u_texture",
-    dimension: "2d",
-  },
-])
-
-// buffers
-
-let system_uniforms_buffer = new Buffer()
-system_uniforms_buffer.uniformBuffer(
-  system_bind_group_layout.getBindGroupSize(),
-)
-
-let mesh_uniforms_buffer = new Buffer()
-mesh_uniforms_buffer.uniformBuffer(mesh_bind_group_layout.getBindGroupSize())
-
-// bindings
-
-let system_uniform_bind_group = new BindGroup({
-  layout: system_bind_group_layout.gpuBindGroupLayout,
-  resources: [
-    {
-      buffer: system_uniforms_buffer.gpuBuffer,
-      offset: 0,
-      size: system_bind_group_layout.getBindGroupSize(),
-    },
-  ],
-})
-
-let uv_sampler = new Sampler()
-let uv_image = document.createElement("img")
-uv_image.src = "/assets/uv.jpg"
-await uv_image.decode()
-let uv_texture = new Texture(null, uv_image)
-
-let mesh_uniform_bind_group = new BindGroup({
-  layout: mesh_bind_group_layout.gpuBindGroupLayout,
-  resources: [
-    {
-      buffer: mesh_uniforms_buffer.gpuBuffer,
-      offset: 0,
-      size: mesh_bind_group_layout.getBindGroupSize(),
-    },
-    uv_sampler.gpuSampler,
-    uv_texture.gpuTexture.createView(),
-  ],
-})
-
-// geometry
-
-let model_matrix = mat4.create()
-
-let geometry = cube()
-let geometry_vertex_buffer = new Buffer()
-let geometry_index_buffer = new Buffer()
-
-geometry_vertex_buffer.vertexBuffer(
-  typed_array_interleave(
-    Float32Array,
-    [3, 3, 2],
-    geometry.positions,
-    geometry.normals,
-    geometry.uvs,
-  ),
-)
-geometry_index_buffer.indexBuffer(new Uint16Array(geometry.cells))
-
-// pipeline
-
-let pipeline = new Pipeline({
-  bindGroupLayouts: [system_bind_group_layout, mesh_bind_group_layout],
-  ins: [
-    new Attribute("position", "vec3"),
-    new Attribute("normal", "vec3"),
-    new Attribute("uv", "vec2"),
-  ],
-  outs: [
-    WGSLBuiltIn.Position,
-    new Attribute("v_normal", "vec3"),
-    new Attribute("v_uv", "vec2"),
-  ],
-
-  vertex: /* wgsl */ `
-var output: Output;
-output.v_normal = normal;
-output.v_uv = uv;
-
-output.position = system.projection_matrix * system.view_matrix * mesh.model_matrix * vec4<f32>(position, 1.0);
-
-return output;
-`,
-  fragment: /* wgsl */ `
-var out_color: vec4<f32>;
-out_color = textureSample(u_texture, u_sampler, v_uv);
-return out_color;
-`,
-})
-
-// command
-
-let clear_command = new Command({
-  pass: new Pass(
-    "render",
-    [new Attachment({r: 0.07, g: 0.07, b: 0.07, a: 1}, {})],
-    new Attachment(1, {}),
-  ),
-})
-let draw_geometry_command = new Command({
-  pipeline,
-  bindGroups: [system_uniform_bind_group, mesh_uniform_bind_group],
-  vertexBuffers: [geometry_vertex_buffer],
-  indexBuffer: geometry_index_buffer,
-  indexFormat: GPUIndexFormat.Uint16,
-  count: geometry.cells.length,
-})
-
 // ecs
-
-let res = Component.ref<Context>()
 
 let clear: App.System = world => {}
 
 let players = Selector.make(IsPlayer)
 
 let draw: App.System = world => {
-  camera_controls.update()
-  camera.position = camera_controls.position
-  camera.target = camera_controls.target
-  camera.update()
+  let time = world.get_resource(Time.res).t_mono()
 
-  system_uniforms_buffer.setSubData(
-    0,
-    typed_array_concat(
-      Float32Array,
-      camera.projectionMatrix,
-      camera.viewMatrix,
-    ),
-  )
-  mesh_uniforms_buffer.setSubData(0, model_matrix)
+  // clear contents of the drawing buffer
+  regl.clear({
+    color: [0, 0, 0, 0],
+    depth: 1,
+  })
 
-  context.render(() => {
-    context.submit(clear_command, () => {
-      context.submit(draw_geometry_command)
-    })
+  // draw a triangle using the command defined above
+  drawTriangle({
+    color: [
+      Math.cos(time * 0.001),
+      Math.sin(time * 0.0008),
+      Math.cos(time * 0.003),
+      1,
+    ],
   })
 }
 
@@ -243,7 +80,6 @@ let render = Range.make()
 
 export let plugin: App.Plugin = app => {
   app
-    .add_resource(res, context)
     .add_system(clear, System.when(render))
     .add_system(draw, System.when(render), System.after(clear))
 }
